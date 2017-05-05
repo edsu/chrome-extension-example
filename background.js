83bf5cc59080c19ac417e70de033a055b16bb1a1
Ed Summers <ehs@pobox.com>
2017-05-05 11:11:59 -0400

    initial

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(null, {file: "lib/readability.js"});
+  chrome.tabs.executeScript(null, {file: "lib/jquery.js"});
+  chrome.tabs.executeScript(null, {file: "lib/poet.js"});
+});
